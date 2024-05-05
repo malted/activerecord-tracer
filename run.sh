fa07b2a5305a39680019699c2b42e7973a845166
Malted <malted@malted.dev>
2024-05-05 15:10:29 -0400

    Prepend intermediary assets with _

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-ruby render.rb > res/out.ppm
-convert res/out.ppm res/out.png
-dot res/recursion.dot -T png -o ast.png
+ruby render.rb > res/_out.ppm
+convert res/_out.ppm res/out.png
+dot res/_ast.dot -T png -o ast.png
 
